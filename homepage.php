1be36dda2df7d2803daaaa6dd7e5b4ece9b5b3f3
flynep_neptune <z861092684@gmail.com>
2024-08-10 00:50:32 +0900

    nee

---
+++ homepage.php
@@ -0,0 +1,43 @@
+<?php
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>无标题文档</title>
+</head>
+<link href="css/index.css" rel="stylesheet" type="text/css">
+<body>
+
+<div class="background">
+    <div class="container">
+        <h1 class="main-title">CREEEEEEE EEEEEE</h1>
+        <h2 class="sub-title">ssssss sssssssss</h2>
+        <div class="middle-container">
+            <div class="left-side">
+                <div class="avatar"></div>
+                <div class="content">
+                    <p>春眠不觉晓</p>
+                    <p>处处闻啼鸟</p>
+                    <p>夜来风雨声</p>
+                    <p>花落知多少</p>
+                </div>
+                <div class="footer">好诗好诗</div>
+            </div>
+            <div class="right-container">
+                <div class="photoes"></div>
+                <div class="page-navi"></div>
+            </div>
+        </div>
+        <div class="after-container">
+
+        </div>
+        <footer>
+            <p></p>
+            <div class="decration"></div>
+        </footer>
+    </div>
+</div>    
+
+</body>
+</html>
