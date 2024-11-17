0a869f334cd5f475fafe88f885ba5fdc287eebde
flynep <z861092684@gmail.com>
2024-11-17 23:23:42 +0900

    新增 .gitignore 文件，包含常见的本地文件和目录忽略规则。

---
+++ src/components/app.vue
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+
+<template>
+    <div class="todo-app">
+        <div class="title"> ToDo App </div>
+
+        <div class="todo-form">
+            <input type="text" class="todo-input" placeholder="Add a todo">
+            <div class ="todo-btn">Add ToDo</div>
+        </div>
+
+        <div class="completed">
+            <div>
+                <input type="checkbox" name="" id="">
+                <span class="name">吃饭sss</span>
+            </div>
+
+            <div class="del">chifan </div>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
